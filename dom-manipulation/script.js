839a11ac0a15c3dc44a34b9f82658daa2a163068
Anita Samuel <migist2@gmail.com>
2025-08-13 12:15:36 +0100

    dom manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -113,3 +113,17 @@ function importFromJsonFile(event) {
   };
   fileReader.readAsText(event.target.files[0]);
 }
+
+function exportToJsonFile() {
+  const dataStr = JSON.stringify(quotes, null, 2);
+  const dataBlob = new Blob([dataStr], { type: 'application/json' });
+  const url = URL.createObjectURL(dataBlob);
+  
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'quotes.json';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
